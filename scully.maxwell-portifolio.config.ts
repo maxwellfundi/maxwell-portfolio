2daa038ca272539860b04f78067dabf392bce52a
maxwellfundi <fundimaxwell@gmail.com>
2021-01-27 12:04:04 +0300

    first commit

---
+++ scully.maxwell-portifolio.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "maxwell-portifolio",
+  outDir: './dist/static',
+  routes: {
+  }
+};
